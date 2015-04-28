f6cc45e41b71a5d82d138f9d87838d55b798cc74
Gregg Tavares <github@greggman.com>
2015-04-28 15:51:53 +0900

    update dll and fix color issue

--- Assets/HappyFunTimes/Scripts/ExampleSimplePlayer.cs
+++ Assets/HappyFunTimes/Scripts/ExampleSimplePlayer.cs
@@ -10,37 +10,19 @@ namespace HappyFunTimesExample {
 class ExampleSimplePlayer : MonoBehaviour {
     // Classes based on MessageCmdData are automatically registered for deserialization
     // by CmdName.
-    [CmdName("color")]
     private class MessageColor : MessageCmdData {
         public string color = "";    // in CSS format rgb(r,g,b)
     };
 
-    [CmdName("move")]
     private class MessageMove : MessageCmdData {
         public float x = 0;
         public float y = 0;
     };
 
-    [CmdName("setName")]
-    private class MessageSetName : MessageCmdData {
-        public MessageSetName() {  // needed for deserialization
-        }
-        public MessageSetName(string _name) {
-            name = _name;
-        }
-        public string name = "";
-    };
-
-    [CmdName("busy")]
-    private class MessageBusy : MessageCmdData {
-        public bool busy = false;
-    }
-
     // NOTE: This message is only sent, never received
     // therefore it does not need a no parameter constructor.
     // If you do receive one you'll get an error unless you
     // add a no parameter constructor.
-    [CmdName("scored")]
     private class MessageScored : MessageCmdData {
         public MessageScored(int _points) {
             points = _points;
@@ -50,38 +32,68 @@ class ExampleSimplePlayer : MonoBehaviour {
     }
 
     void InitializeNetPlayer(SpawnInfo spawnInfo) {
+        // Save the netplayer object so we can use it send messages to the phone
         m_netPlayer = spawnInfo.netPlayer;
+
+        // Register handler to call if the player disconnects from the game.
         m_netPlayer.OnDisconnect += Remove;
+        m_netPlayer.OnNameChange += ChangeName;
 
         // Setup events for the different messages.
-        m_netPlayer.RegisterCmdHandler<MessageColor>(OnColor);
-        m_netPlayer.RegisterCmdHandler<MessageMove>(OnMove);
-        m_netPlayer.RegisterCmdHandler<MessageSetName>(OnSetName);
-        m_netPlayer.RegisterCmdHandler<MessageBusy>(OnBusy);
+        m_netPlayer.RegisterCmdHandler<MessageMove>("move", OnMove);
+        m_netPlayer.RegisterCmdHandler<MessageColor>("color", OnColor);
 
         ExampleSimpleGameSettings settings = ExampleSimpleGameSettings.settings();
         m_position = new Vector3(m_rand.Next(settings.areaWidth), 0, m_rand.Next(settings.areaHeight));
         transform.localPosition = m_position;
 
-        SetName(spawnInfo.name);
+        SetName(m_netPlayer.Name);
     }
 
     void Start() {
         m_renderer = gameObject.GetComponent<Renderer>();
         m_position = gameObject.transform.localPosition;
-        m_color = new Color(0.0f, 1.0f, 0.0f);
+        SetColor(new Color(0.0f, 1.0f, 0.0f));
     }
 
-    public void Update() {
+    void Update() {
     }
 
-    private void SetName(string name) {
+    void OnGUI()
+    {
+        Vector2 size = m_guiStyle.CalcSize(m_guiName);
+        Vector3 coords = Camera.main.WorldToScreenPoint(transform.position);
+        m_nameRect.x = coords.x - size.x * 0.5f - 5.0f;
+        m_nameRect.y = Screen.height - coords.y - 30.0f;
+        GUI.Box(m_nameRect, m_name, m_guiStyle);
+    }
+
+    void SetName(string name) {
         m_name = name;
+        gameObject.name = "Player-" + m_name;
+        m_guiName = new GUIContent(m_name);
+        m_guiStyle.normal.textColor = Color.black;
+        m_guiStyle.contentOffset = new Vector2(4.0f, 2.0f);
+        Vector2 size = m_guiStyle.CalcSize(m_guiName);
+        m_nameRect.width  = size.x + 12;
+        m_nameRect.height = size.y + 5;
+    }
+
+    void SetColor(Color color)
+    {
+        m_color = color;
+        m_renderer.material.color = m_color;
+        Color[] pix = new Color[1];
+        pix[0] = color;
+        Texture2D tex = new Texture2D(1, 1);
+        tex.SetPixels(pix);
+        tex.Apply();
+        m_guiStyle.normal.background = tex;
     }
 
     public void OnTriggerEnter(Collider other) {
         // Because of physics layers we can only collide with the goal
-        m_netPlayer.SendCmd(new MessageScored(m_rand.Next(5, 15)));
+        m_netPlayer.SendCmd("scored", new MessageScored(m_rand.Next(5, 15)));
     }
 
     private void Remove(object sender, EventArgs e) {
@@ -89,8 +101,7 @@ class ExampleSimplePlayer : MonoBehaviour {
     }
 
     private void OnColor(MessageColor data) {
-        m_color = CSSParse.Style.ParseCSSColor(data.color);
-        m_renderer.material.color = m_color;
+        SetColor(CSSParse.Style.ParseCSSColor(data.color));
     }
 
     private void OnMove(MessageMove data) {
@@ -101,16 +112,8 @@ class ExampleSimplePlayer : MonoBehaviour {
         gameObject.transform.localPosition = m_position;
     }
 
-    private void OnSetName(MessageSetName data) {
-        if (data.name.Length == 0) {
-            m_netPlayer.SendCmd(new MessageSetName(m_name));
-        } else {
-            SetName(data.name);
-        }
-    }
-
-    private void OnBusy(MessageBusy data) {
-        // not used.
+    private void ChangeName(object sender, EventArgs e) {
+        SetName(m_netPlayer.Name);
     }
 
     private System.Random m_rand = new System.Random();
@@ -119,6 +122,9 @@ class ExampleSimplePlayer : MonoBehaviour {
     private Vector3 m_position;
     private Color m_color;
     private string m_name;
+    private GUIStyle m_guiStyle = new GUIStyle();
+    private GUIContent m_guiName = new GUIContent("");
+    private Rect m_nameRect = new Rect(0,0,0,0);
 }
 
 }  // namespace HappyFunTimesExample
